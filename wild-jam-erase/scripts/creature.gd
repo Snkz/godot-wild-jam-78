be4f9eb620b6ae30b54d4a392a5ffedb3df30eca
= <=>
2025-02-15 00:31:03 -0800

    feat: add texture to creature

--- wild-jam-erase/scripts/creature.gd
+++ wild-jam-erase/scripts/creature.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 func _mouse_enter() -> void:
-	print("hello", self.name)
+	print("hello ", self.name)
 	
 func _mouse_exit() -> void:
-	print("bye", self.name)
+	print("bye ", self.name)
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouse:
