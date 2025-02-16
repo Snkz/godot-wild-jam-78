768fdfdf7fdfa0f2ac72c1c525f333400e4dc55e
Abdi Dahir <abdi.dahir@outlook.com>
2025-02-16 15:45:36 -0800

    feat: mask radius indepant

--- wild-jam-erase/scripts/mask.gd
+++ wild-jam-erase/scripts/mask.gd
@@ -6,9 +6,8 @@ var collision: CollisionShape2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = self.get_node("../../player")
-	collision = player.get_node("CollisionShape2D")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	material.set_shader_parameter("holeCenter", player.global_position)
-	material.set_shader_parameter("holeRadius", collision.shape.radius)
+	material.set_shader_parameter("holeRadius", player.mask_radius)
