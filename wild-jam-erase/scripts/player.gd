2f1d639c6a3ca2aa36422ba445dfd90f1f3ca0ac
= <=>
2025-02-15 16:28:49 -0800

    feat: cutout increases on mouse press

--- wild-jam-erase/scripts/player.gd
+++ wild-jam-erase/scripts/player.gd
@@ -4,6 +4,14 @@ signal creature_selected(Creature, Vector2)
 var nearest_selection = null
 var nearest_threshold = 1.0
 var active_bodies = []
+var collision_shape: CollisionShape2D
+var original_radius = 10.0
+var increased_radius = 150.0
+var lerp_speed = 10.0
+var is_increasing = false
+
+func _ready() -> void:
+	collision_shape = $CollisionShape2D
 
 func set_nearest() -> void:
 	var current_selection = null
@@ -20,7 +28,7 @@ func set_nearest() -> void:
 			
 		nearest_selection = current_selection
 		nearest_selection.emit_signal("nearest_creature_highlighted", true)
-		
+
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouse:
 		if event.is_pressed() and nearest_selection:
@@ -38,7 +46,20 @@ func _on_body_exited(body: Node2D) -> void:
 		nearest_selection.emit_signal("nearest_creature_highlighted", false)
 		nearest_selection = null
 		
-func _physics_process(delta: float) -> void:	
-	var mouseposition = get_viewport().get_mouse_position()
-	self.position = mouseposition
+func _physics_process(delta: float) -> void:
+	var mouse_position = get_viewport().get_mouse_position()
+	self.position = lerp(self.position, mouse_position, lerp_speed * delta)
 	set_nearest()
+	
+	if is_increasing:
+		collision_shape.shape.radius = lerp(collision_shape.shape.radius, increased_radius, lerp_speed * delta)
+	elif not is_increasing:
+		collision_shape.shape.radius = lerp(collision_shape.shape.radius, original_radius, lerp_speed * delta)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				is_increasing = true
+			else:
+				is_increasing = false
